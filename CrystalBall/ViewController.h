8856067f36013de6b106eceff4961b195b1c74b0
Jason Koceja <jason@koceja.com>
2013-08-19 15:36:46 -0700

    Initial Commit

---
+++ CrystalBall/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CrystalBall
+//
+//  Created by Jason Koceja on 8/19/13.
+//  Copyright (c) 2013 Jason Koceja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CrystalBall/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CrystalBall
+//
+//  Created by Jason Koceja on 8/19/13.
+//  Copyright (c) 2013 Jason Koceja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
